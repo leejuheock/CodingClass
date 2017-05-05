aac6af2cf8d5b4e24e45f0b9ec44945c14e537ec
leejuheock <justine120688@gmail.com>
2017-05-05 14:48:30 +0900

    up&down. 2

--- 2.업&다운/1.UP_DOWN.py
+++ 2.업&다운/1.UP_DOWN.py
@@ -8,25 +8,21 @@ while True:
 
     print('up&down 게임입니다 1~100까지의 숫자 중 써보십시오.')
 
-    b = input('')
-
-    v = v+1
+    b = input('') 
 
     c = int ( b )
-
-    if a == c:
-        print('맞았습니다')
-        print( '%d번만에 맞히셧습니다' % v )        
    
+    if 100 >= c > 0:
+        v = v+1
+        if a > c:
+            print('업')
+        if a < c:
+            print('다운')
+        if a == c:
+            print('맞았습니다.')
+            print('%d번만에 맞히셧습니다' % v)
     else:
-        print('잘못 입력하였습니다.1에서 100가지 중 숫자를 입력해주세요.')
-
-    if a > c:
-        print('업')
-
-    if a < c:
-        print('다운')
-        
+        print('1에서 100가지 중 숫자를 입력해주세요.')
         
         
     
