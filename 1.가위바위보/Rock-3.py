d983e3da27eaa65dad7c8d891ec1b09d638034ef
leejuheock <justine120688@gmail.com>
2017-05-05 13:00:11 +0900

    업&다운

--- 1.가위바위보/Rock-3.py
+++ 1.가위바위보/Rock-3.py
@@ -15,11 +15,11 @@ while True:
         break
     
     if a == d:
-        print('비겻다')
+        print('비겻습니다')
     if (a == 1 and d == 2) or (a == 2 and d == 3) or (a == 3 and d == 1 ):
-        print('니가 이겻다')
+        print('당신이 이겻습니다')
     if (a == 2 and d == 1) or (a == 3 and d == 2) or (a == 1 and d == 3):
-        print('내가 이겻다')
+        print('제가 이겻습니다')
 
     if a == 1:
         x = '가위'
--- 2.업&다운/1.UP_DOWN.py
+++ 2.업&다운/1.UP_DOWN.py
@@ -6,24 +6,36 @@ v = 0
 
 while True:
 
-    print('up&down 게임입니다 1~100까지의 숫자 중 써보십시오.')
+    print('up&down 게임입니다.')
+
+    print('1~100까지 숫자 중 하나를 적어주세요')
 
     b = input('')
 
     v = v+1
 
     c = int ( b )
-
+        
     if a == c:
         print('맞았습니다')
         print( '%d번만에 맞히셧습니다' % v )        
         break
+    
+    else:
+        print('잘못 입력하였습니다.')
     if a > c:
         print('업')
 
     if a < c:
         print('다운')
         
+
+    
+    
+            
+     
+        
+    
         
         
     
