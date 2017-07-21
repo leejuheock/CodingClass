028a3c51470ca1adfb2878e52bb35f8a0c152905
leejuheock <justine120688@gmail.com>
2017-07-21 21:29:05 +0900

    FallBall.3

--- FallBall/FallBall.py
+++ FallBall/FallBall.py
@@ -4,31 +4,51 @@ pygame.init()
 size = width,height = 1024, 600
 bgcolor = 150, 0, 150
 gravity = 10
+fps = 23
 
 screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
 
 class Ball():
    
     speed_x = 0
     speed_y = 0
     power = 0.9
-
-    def __init__(self):  
+    
+    #init = CHOGIHWA
+    def __init__(self, start_x, start_y):  
         self.image = pygame.image.load("ball.bmp")
         self.image_location = self.image.get_rect()
+        self.image_location = self.image_location.move((start_x, start_y))
+        
+
     def move(self): 
         self.speed_y = self.speed_y + gravity
         if self.image_location.bottom > height:
             self.speed_y = int(-self.power * (self.speed_y - gravity))
-        self.image_location = self.image_location.move((self.speed_x, self.speed_y))
+            self.image_location = self.image_location.move((self.speed_x, 0))
+            self.image_location.bottom = height
+        else:
+            self.image_location = self.image_location.move((self.speed_x, self.speed_y))
 
-ball_1 = Ball()
-
+ball_list = []
+ball_list.append(Ball(200,0))
+ball_list.append(Ball(200,0))
+ball_list.append(Ball(200,0))
 while True:
+     # fps setting
+     clock.tick(fps)
+     
+     # event wpdj
      for event in pygame.event.get():
          if event.type == pygame.QUIT:
              sys.exit()
+     # Game staitus  update
+     for b in ball_list
+         b.move()
+     ball_list[0].move()
+     # HwaMyun update 
      screen.fill(bgcolor)
-     ball_1.move()
-     screen.blit(ball_1.image, ball_1.image_location)
-     pygame.display.flip()
+     for b in ball_list:
+         screen.blit(b.image, b.image_location)
+      pygame.display.flip()
