ebb70bd80c4a51e259e87317bd6149af1914be7a
leejuheock <justine120688@gmail.com>
2017-08-01 20:57:57 +0900

    한국어로 FallBall 수정

--- FallBall/FallBall.py
+++ FallBall/FallBall.py
@@ -1,10 +1,13 @@
 import sys, pygame
+import random
+
 pygame.init()
 
 size = width,height = 1024, 600
 bgcolor = 150, 0, 150
 gravity = 10
 fps = 23
+max_ball_count = 5
 
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
@@ -15,40 +18,47 @@ class Ball():
     speed_y = 0
     power = 0.9
     
-    #init = CHOGIHWA
-    def __init__(self, start_x, start_y):  
+    #init = 초기화
+
+    def __init__(self, start_x, start_y, speed_x=0, speed_y = 0):  
         self.image = pygame.image.load("ball.bmp")
         self.image_location = self.image.get_rect()
         self.image_location = self.image_location.move((start_x, start_y))
+        self.speed_x = speed_x
+        self.speed_y = speed_y
         
 
     def move(self): 
         self.speed_y = self.speed_y + gravity
         if self.image_location.bottom > height:
-            self.speed_y = int(-self.power * (self.speed_y - gravity))
+            self.speed_y = int(-1*self.power * (self.speed_y)) +  gravity
             self.image_location = self.image_location.move((self.speed_x, 0))
             self.image_location.bottom = height
         else:
             self.image_location = self.image_location.move((self.speed_x, self.speed_y))
+        self.speed_y = self.speed_y + gravity
 
-ball_list = []
-ball_list.append(Ball(200,0))
-ball_list.append(Ball(200,0))
-ball_list.append(Ball(200,0))
+ball_list=[]
+
+
 while True:
-     # fps setting
+     # fps 설정
      clock.tick(fps)
      
-     # event wpdj
+     # 이벤트 제어
      for event in pygame.event.get():
          if event.type == pygame.QUIT:
              sys.exit()
-     # Game staitus  update
-     for b in ball_list
+
+     # 게임 상태 업데이트
+     if len(ball_list) < max_ball_count:	
+         ball_list.append(Ball(0, random.randint(0, 300), random.randint(1, 10)))        
+
+     for b in ball_list:
          b.move()
-     ball_list[0].move()
-     # HwaMyun update 
+
+     # 화면 업데이트
      screen.fill(bgcolor)
      for b in ball_list:
          screen.blit(b.image, b.image_location)
-      pygame.display.flip()
+     pygame.display.flip()
